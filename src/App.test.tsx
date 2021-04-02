6ee04b4007c9c2266b0517d59612f0e80dde6162
Shane32 <shane@acdmail.com>
2021-04-02 17:31:52 -0400

    Separate tests out more

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,17 +3,14 @@ import App from './App';
 import ControlApp from './ControlApp';
 import { render } from '@testing-library/react';
 import { generateImage } from 'jsdom-screenshot';
+import ControlAppCss from './ControlAppCss';
+import AppCss from './AppCss';
+import ControlAppFont from './ControlAppFont';
+import AppFont from './AppFont';
+import ControlAppImage from './ControlAppImage';
+import AppImage from './AppImage';
 
-it('renders app', async () => {
-    render(<App/>);
-    const screenshot = await generateImage({
-        viewport: {
-            height: 800,
-            width: 500
-        }
-    });
-    expect(screenshot).toMatchImageSnapshot();
-})
+jest.setTimeout(10000);
 
 it('renders basic sample', async () => {
     render(<p>test</p>);
@@ -26,7 +23,7 @@ it('renders basic sample', async () => {
     expect(screenshot).toMatchImageSnapshot();
 });
 
-it('renders control sample', async () => {
+it('renders ControlApp', async () => {
     render(<ControlApp/>);
     const screenshot = await generateImage({
         viewport: {
@@ -36,3 +33,80 @@ it('renders control sample', async () => {
     });
     expect(screenshot).toMatchImageSnapshot();
 });
+
+it('renders App', async () => {
+    render(<App/>);
+    const screenshot = await generateImage({
+        viewport: {
+            height: 800,
+            width: 500
+        }
+    });
+    expect(screenshot).toMatchImageSnapshot();
+})
+
+it('renders ControlAppCss', async () => {
+    render(<ControlAppCss/>);
+    const screenshot = await generateImage({
+        viewport: {
+            height: 800,
+            width: 500
+        }
+    });
+    expect(screenshot).toMatchImageSnapshot();
+});
+
+it('renders AppCss', async () => {
+    render(<AppCss/>);
+    const screenshot = await generateImage({
+        viewport: {
+            height: 800,
+            width: 500
+        }
+    });
+    expect(screenshot).toMatchImageSnapshot();
+})
+
+it('renders ControlAppFont', async () => {
+    render(<ControlAppFont/>);
+    const screenshot = await generateImage({
+        viewport: {
+            height: 800,
+            width: 500
+        }
+    });
+    expect(screenshot).toMatchImageSnapshot();
+});
+
+it('renders AppFont', async () => {
+    render(<AppFont/>);
+    const screenshot = await generateImage({
+        viewport: {
+            height: 800,
+            width: 500
+        }
+    });
+    expect(screenshot).toMatchImageSnapshot();
+})
+
+it('renders ControlAppImage', async () => {
+    render(<ControlAppImage/>);
+    const screenshot = await generateImage({
+        viewport: {
+            height: 800,
+            width: 500
+        }
+    });
+    expect(screenshot).toMatchImageSnapshot();
+});
+
+it('renders AppImage', async () => {
+    render(<AppImage/>);
+    const screenshot = await generateImage({
+        viewport: {
+            height: 800,
+            width: 500
+        }
+    });
+    expect(screenshot).toMatchImageSnapshot();
+})
--- src/AppCss.tsx
+++ src/AppCss.tsx
@@ -2,13 +2,9 @@
 import styles from "./App.module.scss";
 import "./scss/styles.scss";
 import "./index.css";
-import cat from "./images/cat.jpg";
-import visa from "./images/visa.svg";
-import wifi from "./images/wifi.png";
 
-const App = () => {
+const AppCss = () => {
     return (<>
-        <p style={{fontFamily:"Train One", fontSize:40}}>FONT SAMPLE</p>
         <table>
             <tbody>
             <tr>
@@ -29,22 +25,12 @@ const App = () => {
             </tr>
             <tr>
                 <th>Inline</th>
-                <th>Cat (jpg)</th>
             </tr>
             <tr>
                 <td><p style={{ color: "blue" }}>Blue on White</p></td>
-                <td><img src={cat} width={200} alt="cat"/></td>
-            </tr>
-            <tr>
-                <th>Visa (svg)</th>
-                <th>WiFi (png)</th>
-            </tr>
-            <tr>
-                <td><img src={visa} width={200} alt="visa"/></td>
-                <td><img src={wifi} width={200} alt="wifi"/></td>
             </tr>
             </tbody>
         </table></>);
 }
 
-export default App;
+export default AppCss;
